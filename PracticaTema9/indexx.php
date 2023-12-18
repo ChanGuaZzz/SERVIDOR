43b1aa26265aea6562974de67f4c0e7769f83643
ChanGuaZzz <gga0006@alu.medac.es>
2023-12-18 15:19:27 +0100

    avance

--- PracticaTema9/indexx.php
+++ PracticaTema9/indexx.php
@@ -1,7 +1,7 @@
 <html lang="en">
 
 <head>
-    <link rel="stylesheet" href="estilo/style.css">
+    <link rel="stylesheet" href="estiloo/style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
